185277ba204de706c4892f748fdd73c54723da42
Sebastian <sebastian.sarviala@gmail.com>
2023-12-20 11:32:18 +0200

    Cors added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'express-async-errors';
+import cors from 'cors';
 
 import diaryRouter from './routes/diaries';
 import { errorHandler } from './middleware';
@@ -9,6 +9,7 @@ const PORT = 3000;
 const BASE_URL = `http://localhost:${PORT}`;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/diaries', diaryRouter);
 app.use(errorHandler);
 
